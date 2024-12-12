cbee002abc8afd384dcce8dbf149c21775ab000b
xiaoxiae <tomas@slama.dev>
2024-12-12 13:59:42 +0100

    Day 11: prettier

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -1,17 +1,16 @@
-use rustc_hash::{FxHashMap as HashMap};
 use crate::util::Day;
+use rustc_hash::FxHashMap as HashMap;
 
 pub struct Day11;
 
 /// A wrapper for caching stone results.
 fn _count_stone(stone: i64, iterations: i64, cache: &mut HashMap<(i64, i64), i64>) -> i64 {
-    let key = (stone, iterations);
-    if cache.contains_key(&key) {
-        return *cache.get(&key).unwrap();
+    if let Some(&result) = cache.get(&(stone, iterations)) {
+        result
     } else {
         let result = count_stone(stone, iterations, cache);
-        cache.insert(key, result);
-        return result;
+        cache.insert((stone, iterations), result);
+        result
     }
 }
 
@@ -22,41 +21,52 @@ fn count_stone(stone: i64, iterations: i64, cache: &mut HashMap<(i64, i64), i64>
 
     match stone {
         0 => _count_stone(stone + 1, iterations - 1, cache),
-        v if (v.ilog10() + 1) % 2 == 0 => {
-            let digits = (v.ilog10() + 1) / 2;
+        v => {
+            let digits = v.ilog10() + 1;
 
-            _count_stone(stone / 10_i64.pow(digits), iterations - 1, cache)
-                + _count_stone(stone % 10_i64.pow(digits), iterations - 1, cache)
+            if digits % 2 == 0 {
+                let offset = 10_i64.pow(digits / 2);
+
+                _count_stone(stone / offset, iterations - 1, cache)
+                    + _count_stone(stone % offset, iterations - 1, cache)
+            } else {
+                _count_stone(v * 2024, iterations - 1, cache)
+            }
         }
-        v => _count_stone(v * 2024, iterations - 1, cache)
     }
 }
 
 impl Day for Day11 {
     fn solve_part1(&self, input: &str) -> Option<String> {
-        let stones = input.trim().split_whitespace()
-            .map(|n| n.parse::<i64>().unwrap()).collect::<Vec<i64>>();
+        let stones = input
+            .trim()
+            .split_whitespace()
+            .map(|n| n.parse::<i64>().unwrap())
+            .collect::<Vec<i64>>();
 
         let mut stone_cache: HashMap<(i64, i64), i64> = HashMap::default();
 
-        let mut total = 0;
-        for s in stones {
-            total += count_stone(s, 25, &mut stone_cache);
-        }
+        let total = stones
+            .iter()
+            .map(|s| count_stone(*s, 25, &mut stone_cache))
+            .sum::<i64>();
 
         Option::from(total.to_string())
     }
 
     fn solve_part2(&self, input: &str) -> Option<String> {
-        let stones = input.trim().split_whitespace()
-            .map(|n| n.parse::<i64>().unwrap()).collect::<Vec<i64>>();
+        let stones = input
+            .trim()
+            .split_whitespace()
+            .map(|n| n.parse::<i64>().unwrap())
+            .collect::<Vec<i64>>();
 
         let mut stone_cache: HashMap<(i64, i64), i64> = HashMap::default();
 
-        let mut total = 0;
-        for s in stones {
-            total += count_stone(s, 75, &mut stone_cache);
-        }
+        let total = stones
+            .iter()
+            .map(|s| count_stone(*s, 75, &mut stone_cache))
+            .sum::<i64>();
 
         Option::from(total.to_string())
     }
