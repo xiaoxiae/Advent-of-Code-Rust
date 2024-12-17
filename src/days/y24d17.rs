415a6bc03f0a609cf877347748b286939ddf2ded
xiaoxiae <tomas@slama.dev>
2024-12-17 16:06:40 +0100

    Day 17: genetic solution

--- src/days/y24d17.rs
+++ src/days/y24d17.rs
@@ -1,6 +1,8 @@
 use crate::util::Day;
 use itertools::Itertools;
 use regex::Regex;
+use rand::Rng;
+use rayon::prelude::*;
 
 type Registers = Vec<usize>;
 type Instructions = Vec<usize>;
@@ -72,7 +74,7 @@ fn run(registers: &mut Registers, instructions: &Instructions) -> Vec<usize> {
 /// - when looping the digit doesn't change the digits after it
 ///
 /// Combining these two facts, we can loop digits from the end and recurse.
-fn brute(value: usize, power: usize, instructions: &Instructions) -> Option<usize> {
+fn recursive(value: usize, power: usize, instructions: &Instructions) -> Option<usize> {
     for i in 0..(1 << 3) {
         let new_value = value + i * (1 << (3 * power));
 
@@ -84,13 +86,76 @@ fn brute(value: usize, power: usize, instructions: &Instructions) -> Option<usiz
                 return Some(new_value);
             }
 
-            if let Some(v) = brute(new_value, power - 1, instructions) {
+            if let Some(v) = recursive(new_value, power - 1, instructions) {
                 return Some(v);
             }
         }
     }
 
     None
+}
+
+/// Flip a random bit
+fn mutate(value: usize, bits: usize) -> usize {
+    let bit_to_flip = rand::thread_rng().gen_range(0..bits);
+    value ^ (1 << bit_to_flip)
+}
+
+/// Fitness of an individual is how many places it matches with instructions
+fn fitness(value: usize, instructions: &Instructions) -> usize {
+    let mut registers = vec![value, 0, 0];
+    let out = run(&mut registers, &instructions);
+
+    out.iter().zip(instructions.iter())
+        .filter(|(&i, &j)| i == j)
+        .count()
+}
+
+/// A genetic solution that randomly flips bits of the register A until we get a quine
+fn genetic(instructions: &Instructions) -> Option<usize> {
+    const PATIENCE: usize = 3_200;  // how many generations without improvement we wait
+    const POOL_SIZE: usize = 1_024;  // generation size
+
+    let mut pool: Vec<usize> = vec![0; POOL_SIZE];
+
+    let mut min_a = usize::MAX;
+    let mut generation = 0;
+    loop {
+        // calculate fitness
+        let mut values: Vec<(usize, usize)> = pool.par_iter()
+            .map(|v| (fitness(*v, instructions), *v))
+            .collect::<Vec<_>>();
+
+        values.sort_by_key(|v| v.0);
+
+        for &(value, a) in &values {
+            if value == instructions.len() && min_a > a {
+                min_a = a;
+                generation = 0;
+            }
+        }
+
+        // kill half the population, mutating the remaining ones
+        let mut offspring = values.iter().map(|v| v.1)
+            .collect::<Vec<usize>>();
+
+        offspring = offspring[offspring.len() / 2..].to_owned()
+            .iter().flat_map(|&v| [mutate(v, instructions.len() * 3), mutate(v, instructions.len() * 3)])
+            .collect::<Vec<usize>>();
+
+        pool = offspring;
+
+        generation += 1;
+
+        if generation > PATIENCE {
+            break;
+        }
+    }
+
+    match min_a {
+        usize::MAX => None,
+        v => Option::from(v),
+    }
 }
 
 pub struct Y24D17;
@@ -107,7 +172,12 @@ impl Day for Y24D17 {
     fn solve_part2(&self, input: &str) -> Option<String> {
         let (_, instructions) = parse_input(input);
 
-        let a = brute(0, instructions.len() - 1, &instructions)
+        // I was bored, so I also solved it with a genetic algorithm
+        //
+        // let a = genetic(&instructions)
+        //     .expect("No A value found!");
+
+        let a = recursive(0, instructions.len() - 1, &instructions)
             .expect("No A value found!");
 
         Option::from(a.to_string())
