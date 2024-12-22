a9e82e453b9e9db56e5f43283cee94305282f1b9
xiaoxiae <tomas@slama.dev>
2024-12-22 14:51:37 +0100

    Day 14: faster

--- src/days/y24d14.rs
+++ src/days/y24d14.rs
@@ -1,24 +1,29 @@
-use regex::Regex;
 use crate::util::Day;
 use rayon::prelude::*;
+use regex::Regex;
 
 static WIDTH: usize = 101;
 static HEIGHT: usize = 103;
 
 static STEPS: usize = 100;
 
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Bot {
     position: (i64, i64),
     velocity: (i64, i64),
 }
 
-
 impl Bot {
     fn step(&mut self, width: usize, height: usize) {
         self.position.0 = (self.position.0 + self.velocity.0 + width as i64) % (width as i64);
         self.position.1 = (self.position.1 + self.velocity.1 + height as i64) % (height as i64);
+    }
+
+    fn step_by(&mut self, steps: usize, width: usize, height: usize) {
+        self.position.0 =
+            (self.position.0 + self.velocity.0 * (steps as i64)).rem_euclid(width as i64);
+        self.position.1 =
+            (self.position.1 + self.velocity.1 * (steps as i64)).rem_euclid(height as i64);
     }
 }
 
@@ -39,8 +44,7 @@ fn parse_input(input: &str) -> Vec<Bot> {
         .collect()
 }
 
-
-fn bot_position_variances(bots: &Vec<Bot>) -> i64 {
+fn bot_position_variance(bots: &Vec<Bot>) -> i64 {
     // Extract x and y coordinates into separate vectors
     let mut x_coords: Vec<i64> = bots.iter().map(|bot| bot.position.0).collect();
     let mut y_coords: Vec<i64> = bots.iter().map(|bot| bot.position.1).collect();
@@ -64,43 +68,27 @@ fn bot_position_variances(bots: &Vec<Bot>) -> i64 {
     x_distance * y_distance
 }
 
-fn _print_robots(bots: &Vec<Bot>) {
-    for y in 0..HEIGHT {
-        for x in 0..WIDTH {
-            let mut count = 0;
-            for bot in bots {
-                if bot.position == (x as i64, y as i64) {
-                    count += 1;
-                }
-            }
-
-            match count {
-                0 => print!("{}", "."),
-                v => print!("{}", v)
-            }
-        }
-        println!()
-    }
-}
-
 pub struct Y24D14;
 
 impl Day for Y24D14 {
     fn solve_part1(&self, input: &str) -> Option<String> {
         let mut bots = parse_input(input);
 
-        let indicators: Vec<_> = bots.par_iter_mut().map(|bot| {
-            for _ in 0..STEPS {
-                bot.step(WIDTH, HEIGHT);
-            }
+        let indicators: Vec<_> = bots
+            .par_iter_mut()
+            .map(|bot| {
+                for _ in 0..STEPS {
+                    bot.step(WIDTH, HEIGHT);
+                }
 
-            return (
-                (bot.position.0 < (WIDTH / 2) as i64) as i64,
-                (bot.position.0 > (WIDTH / 2) as i64) as i64,
-                (bot.position.1 < (HEIGHT / 2) as i64) as i64,
-                (bot.position.1 > (HEIGHT / 2) as i64) as i64,
-            );
-        }).collect();
+                return (
+                    (bot.position.0 < (WIDTH / 2) as i64) as i64,
+                    (bot.position.0 > (WIDTH / 2) as i64) as i64,
+                    (bot.position.1 < (HEIGHT / 2) as i64) as i64,
+                    (bot.position.1 > (HEIGHT / 2) as i64) as i64,
+                );
+            })
+            .collect();
 
         let mut quadrants = vec![0, 0, 0, 0];
         for indicator in indicators {
@@ -120,27 +108,48 @@ impl Day for Y24D14 {
     fn solve_part2(&self, input: &str) -> Option<String> {
         let mut bots = parse_input(input);
 
-        let mut min_dist = i64::MAX;
-        let mut steps = 0;
+        const BATCH_SIZE: usize = 512;
+
+        let mut iteration = 0;
+        let mut min_variance = i64::MAX;
+        let mut min_steps = 0;
+
         loop {
-            bots.iter_mut().for_each(|bot| {
-                bot.step(WIDTH, HEIGHT);
-            });
+            let batch: Vec<_> = (0..BATCH_SIZE)
+                .into_par_iter()
+                .map(|thread_idx| {
+                    let mut local_bots = bots.clone();
 
-            steps += 1;
+                    let steps = thread_idx + iteration * BATCH_SIZE;
 
-            let distances = bot_position_variances(&bots);
-            if distances < min_dist {
-                min_dist = distances;
+                    local_bots.iter_mut().for_each(|bot| {
+                        bot.step_by(steps, WIDTH, HEIGHT);
+                    });
 
-                // this probably a bit of a shit assumption, but it's not a well-formed problem
-                // anyway and works well for my input, so we take those
-                if steps > STEPS {
+                    let variance = bot_position_variance(&local_bots);
+
+                    variance
+                })
+                .collect();
+
+            let min_batch_variance = *batch.iter().min().unwrap();
+
+            if min_batch_variance < min_variance {
+                min_steps = iteration * BATCH_SIZE
+                    + batch.iter().position(|&x| x == min_batch_variance).unwrap();
+
+                // we assume that the variance of the image will be much less than that of random robots
+                // i.e. that it improves by a large amount (>25%; a bit arbitrary but fuck it we ball)
+                if iteration != 0 && (min_batch_variance as f64) < (min_variance as f64) * 1.25 {
                     break;
                 }
+
+                min_variance = min_batch_variance;
             }
+
+            iteration += 1;
         }
 
-        Option::from(steps.to_string())
+        Option::from(min_steps.to_string())
     }
 }
