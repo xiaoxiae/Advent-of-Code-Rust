c1cd85152ef4b8293e3e74ac4324b21c021c6cf2
xiaoxiae <tomas@slama.dev>
2025-03-03 00:12:12 +0100

    y17d15: faster!

--- src/y17/d15.rs
+++ src/y17/d15.rs
@@ -1,6 +1,15 @@
+use std::ptr::replace;
+use rayon::join;
 use crate::util::Day;
 
 pub struct D15;
+
+// https://en.wikipedia.org/wiki/Lehmer_random_number_generator#Sample_C99_code
+fn lcg_parkmiller(state: &mut usize, multiplier: usize) {
+    let product = (*state) * (multiplier);
+    let x = (product & 0x7fffffff) + (product >> 31);
+    *state = (x & 0x7fffffff) + (x >> 31);
+}
 
 impl Day for D15 {
     fn solve_part1(&self, input: &str) -> Option<String> {
@@ -13,8 +22,8 @@ impl Day for D15 {
         let mut count = 0;
 
         for _ in 0..40_000_000 {
-            a = (a * 16807) % 2147483647;
-            b = (b * 48271) % 2147483647;
+            lcg_parkmiller(&mut a, 16807);
+            lcg_parkmiller(&mut b, 48271);
 
             if (a & 0xFFFF) == (b & 0xFFFF) {
                 count += 1;
@@ -35,7 +44,7 @@ impl Day for D15 {
 
         for _ in 0..5_000_000 {
             loop {
-                a = (a * 16807) % 2147483647;
+                lcg_parkmiller(&mut a, 16807);
 
                 if a % 4 == 0 {
                     break;
@@ -43,7 +52,7 @@ impl Day for D15 {
             }
 
             loop {
-                b = (b * 48271) % 2147483647;
+                lcg_parkmiller(&mut b, 48271);
 
                 if b % 8 == 0 {
                     break;
