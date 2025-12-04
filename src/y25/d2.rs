3328023b12efbc12f2b4c0bc416af6acb40a4e36
xiaoxiae <tomas@slama.dev>
2025-12-04 08:28:26 +0100

    y25d2: faster

--- src/y25/d2.rs
+++ src/y25/d2.rs
@@ -1,77 +1,92 @@
 use crate::util::Day;
-use itertools::Itertools;
-use std::collections::HashMap;
+use rustc_hash::FxHashSet;
 
 pub struct D2;
 
-fn is_invalid(s: usize) -> bool {
-    let d = s.ilog10();
-    let ten = 10_u32.pow((d + 1) / 2) as usize;
-
-    return s % ten == s / ten;
+fn digit_count(n: usize) -> usize {
+    (n.ilog10() + 1) as usize
 }
 
-fn is_really_invalid(mut s: usize) -> bool {
-    let d = s.ilog10();
+///
+/// Build a number from repeating a part.
+///
+fn build_repeated(part: usize, count: usize, d: u32) -> usize {
+    let ten = 10_usize.pow(d);
+    let mut result = part;
+    for _ in 1..count {
+        result = result * ten + part;
+    }
+    result
+}
 
-    for i in 1..=((d + 1) / 2) {
-        let ten = 10_u32.pow(i) as usize;
-        let v = s % ten;
-        let mut ns = s;
+///
+/// Given a number, return smallest build_repeated number smaller than it.
+///
+fn smallest(mut number: usize, parts: usize) -> usize {
+    number = number.max(10);
+    let part_width = (digit_count(number) / parts) as u32;
+    let ten = 10_usize.pow(part_width);
 
-        while ns > 0 {
-            if ns % ten != v {
-                break;
-            }
-
-            ns /= ten;
-        }
-
-        // the divisibility check is important; prevents stuff like 70707
-        if ns == 0 && (d + 1) % i == 0 {
-            return true;
-        }
+    let mut min = usize::MAX;
+    for _ in 0..parts {
+        min = min.min(number % ten);
+        number /= ten;
     }
 
-    false
+    build_repeated(min.max(1), parts, part_width)
+}
+
+///
+/// Return the next repeated number.
+///
+fn next(s: usize, parts: usize) -> usize {
+    let part_width = (digit_count(s) / parts) as u32;
+    let cp = s % 10_usize.pow(part_width) + 1;
+    build_repeated(cp, parts, cp.ilog10() + 1)
 }
 
 impl Day for D2 {
     fn solve_part1(&self, input: &str) -> Option<String> {
-        let mut invalid_id_sum = 0;
+        let mut sum = 0;
 
-        for part in input.trim().split(",") {
+        for part in input.trim().split(',') {
             let (a, b) = part.split_once('-').unwrap();
+            let a: usize = a.parse().unwrap();
+            let b: usize = b.parse().unwrap();
 
-            let a = a.parse().unwrap();
-            let b = b.parse().unwrap();
-
-            for i in a..=b {
-                if is_invalid(i) {
-                    invalid_id_sum += i;
+            let mut cur = smallest(a, 2);
+            while cur <= b {
+                if a <= cur {
+                    sum += cur;
                 }
+                cur = next(cur, 2);
             }
         }
 
-        Some(invalid_id_sum.to_string())
+        Some(sum.to_string())
     }
 
     fn solve_part2(&self, input: &str) -> Option<String> {
-        let mut invalid_id_sum = 0;
+        let mut sum = 0;
 
-        for part in input.trim().split(",") {
+        for part in input.trim().split(',') {
             let (a, b) = part.split_once('-').unwrap();
+            let a: usize = a.parse().unwrap();
+            let b: usize = b.parse().unwrap();
 
-            let a = a.parse().unwrap();
-            let b = b.parse().unwrap();
+            let mut seen = FxHashSet::default();
 
-            for i in a..=b {
-                if is_really_invalid(i) {
-                    invalid_id_sum += i;
+            for parts in 2..=digit_count(b) {
+                let mut cur = smallest(a, parts);
+                while cur <= b {
+                    if a <= cur && seen.insert(cur) {
+                        sum += cur;
+                    }
+                    cur = next(cur, parts);
                 }
             }
         }
 
-        Some(invalid_id_sum.to_string())
+        Some(sum.to_string())
     }
 }
